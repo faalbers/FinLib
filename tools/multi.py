d2aa5420234cf87fb0d0b146f2b69cabcd100dc5
faalbers <chalbers@gmail.com>
2024-08-13 21:58:18 -0700

    fixed cpu count

--- tools/multi.py
+++ tools/multi.py
@@ -2,7 +2,7 @@ from multiprocessing.dummy import Pool
 from multiprocessing import cpu_count
 
 def exec(poolVariables, multiProc):
-    cpuCount = cpu_count() * 4
+    cpuCount = cpu_count()
     multiPool = Pool(cpuCount)
     return multiPool.map(multiProc, poolVariables)
 
