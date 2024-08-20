5c017865427d235653fbb3de6e9d25087a1b383d
faalbers <chalbers@gmail.com>
2024-08-20 11:37:00 -0700

    updated multi with cpu count

--- tools/multi.py
+++ tools/multi.py
@@ -1,8 +1,9 @@
 from multiprocessing.dummy import Pool
 from multiprocessing import cpu_count
 
-def exec(poolVariables, multiProc):
-    cpuCount = cpu_count()
+def exec(poolVariables, multiProc, cpuCount=None):
+    if cpuCount == None:
+        cpuCount = cpu_count()
     multiPool = Pool(cpuCount)
     return multiPool.map(multiProc, poolVariables)
 
